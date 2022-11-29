077214b72c4e2c242707b76eb4212c0e968f041e
Kamilya <kami130531@gmail.com>
2022-11-29 19:38:19 +0300

    Condition

---
+++ EX005_Condition/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Маша")
+{
+    Console.WriteLine("УРА, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}   
+
+
+
+
+
+
+
+    
+
+    
+
+
+   
