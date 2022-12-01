e61bf7576e77915044c8db1f06d0e16a28d13dad
Kamilya <kami130531@gmail.com>
2022-12-01 22:52:44 +0300

    add_EX013_texts/

---
+++ EX013_texts/Program.cs
@@ -0,0 +1,28 @@
+﻿string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винцегероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text,' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text,'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text,'с', 'С');
+Console.WriteLine(newText);
